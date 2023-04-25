61461d18d71a6c359e10cedef48daefcd4244cfa
Sergio <sergioadm120@gmail.com>
2023-04-25 11:17:20 -0300

    Creating an anime listing

--- src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -1,13 +1,19 @@
 package academy.devdojo.springboot2.controller;
 
-import main.java.academy.devdojo.springboot2.domain.Anime;
+import academy.devdojo.springboot2.domain.Anime;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController //Com isso, os retornos dos métodos são apenas strings.
+//@RequestMapping("anime")
+@RequestMapping(value = "/anime")
 public class AnimeController {
+    //@GetMapping(path = "list")
+    @GetMapping("/list")
     public List<Anime> list() {
-        return List.of(new Anime("Dragon Ball Z"));
+        return List.of(new Anime("Dragon Ball Z"), new Anime("Naruto"));
     }
 }
--- src/main/java/academy/devdojo/springboot2/domain/Anime.java
+++ src/main/java/academy/devdojo/springboot2/domain/Anime.java
@@ -1,13 +1,19 @@
-package main.java.academy.devdojo.springboot2.domain;
+package academy.devdojo.springboot2.domain;
 
 public class Anime {
     private String name;
 
     public Anime(String name) {
-
+        this.name = name;
     }
 
-    public Anime() {
+    public Anime() { }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
