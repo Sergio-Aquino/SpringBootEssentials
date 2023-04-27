df4ad1145452b4dc559cc878af2d6a22debdc6b2
Sergio <sergioadm120@gmail.com>
2023-04-26 21:46:00 -0300

    Trying to add database

--- src/main/java/academy/devdojo/springboot2/domain/Anime.java
+++ src/main/java/academy/devdojo/springboot2/domain/Anime.java
@@ -1,11 +1,20 @@
 package academy.devdojo.springboot2.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@Entity
+@NoArgsConstructor
 public class Anime {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String name;
     private Long id;
 }
